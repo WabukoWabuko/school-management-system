84324cfc66f53acc88a9fdcf149c8ac2b67ad618
WabukoWabuko <basilwabbs@gmail.com>
2025-06-03 17:50:35 +0300

    Fixed blank screen by updating index.js for React 18 and verifying components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
